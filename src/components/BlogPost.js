b4c5caa122bf8de98f882e77951ac230c520c256
Conrad Etherington <conrad.etherington@gmail.com>
2022-11-03 07:48:51 -0600

    commit tree 085880a21f8b33c603c73b725c7289eb5b8e9703

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -5,10 +5,14 @@ import Comment from "./Comment";
 function BlogPost() {
   return (
     <div id="blog-post">
-      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
+      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." 
+      isPublished={true}
+      minutesToRead={1}/>
       <Comment commentText="I agree with this statement. - Angela Merkel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
       <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
+      <Comment commentText="Conrad passed this down as a prop!" />
+      <Comment commentText="HOLY GUACAMOLE!" hiddenProp="Conrad hid this prop and put it in the console"/>
     </div>
   );
 }
--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function Comment(props) {
+  console.log(props.hiddenProp)
   return <div className="comment">{props.commentText}</div>;
 }
 
